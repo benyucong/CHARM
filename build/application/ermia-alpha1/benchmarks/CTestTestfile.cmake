7628a8171258e77d7655d78b280da15bc212c7cc
benyucong <benyucong@gmail.com>
2025-05-02 10:16:35 +0300

    update gitignore

--- build/application/ermia-alpha1/benchmarks/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/benyucong/labs/CHARM/application/ermia-alpha1/benchmarks
-# Build directory: /home/benyucong/labs/CHARM/build/application/ermia-alpha1/benchmarks
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("egen")
-subdirs("record")
--- build/application/ermia-alpha1/benchmarks/egen/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/benyucong/labs/CHARM/application/ermia-alpha1/benchmarks/egen
-# Build directory: /home/benyucong/labs/CHARM/build/application/ermia-alpha1/benchmarks/egen
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
